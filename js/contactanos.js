deeae54530c2ca309d526a1279290cf1aba92969
Marihan Itzel <marihan.bernal@gmail.com>
2022-06-28 23:46:08 -0500

    Ajustes de diseño de navbar y responsividad de contactanos

--- js/contactanos.js
+++ js/contactanos.js
@@ -125,20 +125,6 @@ document.getElementById('formC').reset();
 
 mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-}
 
 /* Botón "Ir Arriba" */
 
