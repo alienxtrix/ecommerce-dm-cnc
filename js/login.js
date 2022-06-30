39c69fd01e2ae58a5a3f2e1a321c13f5e40f24b8
jluis <jluis.vacan64@gmail.com>
2022-06-30 12:49:21 -0500

    Integración de inicio de sesión todos los html y eliminado de formproducto.html

--- js/login.js
+++ js/login.js
@@ -1,13 +1,15 @@
 //Trae los inputs del formulario
+let statusSesion = 0;
 let mailContact = document.getElementById("mailContact")
 let passwordContact = document.getElementById("passwordContact")
-
+let usuarios= [];
 
 //------------------------> F U N C I O N E S    D E   V A L I D A C I O N E S <--------------------------------
 
 
 function validarContraseña () {
-    if((confirPassContact.value != passwordContact.value) || confirPassContact.value.length==0) {
+    console.log(confirm);
+    if(passwordContact.value.length==0 || passwordContact.value.length<=4) {
         passwordContact.style.border = "red thin solid";
         document.getElementById("alertPassword").innerHTML = "Inválido, tu contraseña debe contener más de 5 caracteres";
         document.getElementById("alertPassword").style="display: block; margin-bottom: -10px;";
@@ -69,21 +71,50 @@ enviar.addEventListener("click", (event)=> {
           })
      return false;
     }
-    
+
+// Si se accede a correctamente a la cuenta, se cambia login por Mi sesion y redirecciona a index
+let correo;
+let password;
+for (let i=0; i< usuarios.length; i++){
+    correo = usuarios[i].Email;
+    password = usuarios[i].Contrasena;
+    if (correo == mailContact.value && password == passwordContact.value) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Correcto',
+            text: 'Se ha iniciado sesión',
+            showConfirmButton: false,
+            timer: 1500
+        });
+        localStorage.setItem ("statusSesion", correo); 
+        setTimeout(() => {
+            location.href ="/index.html";
+        }, 1500);
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text:'Usuario y/o contraseña incorrecto',
+        })
+    }    
+}
 
 
-    // Si no falla validaciones, se muestra alerta de que se registró correctamente
-    Swal.fire({
-        icon: 'success',
-        title: 'Correcto',
-        text: 'Se ha iniciado sesión',
-        showConfirmButton: false,
-        timer: 1500
-    })
+
 
 // Se limpia formulario
 document.getElementById('formIn').reset();
 mailContact.style.border = "grey thin solid";
 passwordContact.style.border = "grey thin solid";
 
+});
+
+// Función para traer nombre de usuario y email
+window.addEventListener('load', function() {
+    if (localStorage.getItem("contador") != null) {
+        contador = JSON.parse(localStorage.getItem("contador"));
+    } // if
+    if (localStorage.getItem("Usuario") != null) {
+        usuarios = JSON.parse(localStorage.getItem("Usuario"));
+    } // if
 });
