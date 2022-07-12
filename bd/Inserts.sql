23aede49b844cc2dc458d1cfa7b18b329268106c
Roman <jorgerromanb@gmail.com>
2022-07-12 09:22:52 -0500

    Actualizacion de la base de datos, diagrama y del inserts

--- bd/Inserts.sql
+++ bd/Inserts.sql
@@ -1,31 +1,31 @@
-USE `maderas&cnc` ;
+USE `maderacncdb` ;
 -- Insert de categorias
-INSERT INTO categorias ( nombre, descripcion) VALUES ("Almacenamiento", "Productos de madera para almacenamiento.");
-INSERT INTO categorias (nombre, descripcion) VALUES ("Cocina", "Productos de madera para la cocina.");
-INSERT INTO categorias (nombre, descripcion) VALUES ("Decoración", "Productos de madera para decoración.");
-INSERT INTO categorias (nombre, descripcion) VALUES ("Varios", "Productos de madera para usos varios.");
+INSERT INTO categorias (category_name, category_description) VALUES ("Almacenamiento", "Productos de madera para almacenamiento.");
+INSERT INTO categorias (category_name, category_description) VALUES ("Cocina", "Productos de madera para la cocina.");
+INSERT INTO categorias (category_name, category_description) VALUES ("Decoración", "Productos de madera para decoración.");
+INSERT INTO categorias (category_name, category_description) VALUES ("Varios", "Productos de madera para usos varios.");
 -- Insert de productos
-insert into producto (nombre, calificacion, precio, imagen, estatus, descripcion,categorias_idcategorias) 
-values ('Porta Cerveza', 5, 250.00, 'data:image/png;base64,', 'Activo', 'Lleva tus cervezas a todos lados con este porta cervezas muy funcional.', 1);
-insert into producto (nombre,calificacion, precio, imagen, estatus, descripcion, categorias_idcategorias) 
-values ('Bandeja en forma de conejo', 3, 100.00, 'data:image/png;base64,', 'Activo', 'Bandeja con diseño de conejo elaborada 100% madera y barnizada.', 2);
-insert into producto (nombre,calificacion, precio, imagen, estatus, descripcion, categorias_idcategorias) 
-values ('Base para cuchillos', 2, 250.00, 'data:image/png;base64,', 'Activo', 'Porta Cuchillos hecho de madera de pino de primera calidad.', 4);
-insert into producto (nombre,calificacion, precio, imagen, estatus, descripcion, categorias_idcategorias) 
-values ('Base para audífonos', 3, 500.00, 'data:image/png;base64,', 'Activo', 'Base para audífonos y/o auriculares hecho 100% de madera.', 1);
-insert into producto (nombre,calificacion, precio, imagen, estatus, descripcion, categorias_idcategorias) 
-values ('Porta Vaso', 4, 50.00, 'data:image/png;base64,', 'Activo', 'Viste tu mesa y tus bebidas con este paquete de 4 portavasos. Con diseño de rompecabezas.', 4);
+insert into producto (product_name, product_img, product_cost, product_status, product_description, product_rate, product_category) 
+values ('Porta Cerveza', 'data:image/png;base64,', 250.00, 'Activo', 'Lleva tus cervezas a todos lados con este porta cervezas muy funcional.', 5, 1);
+insert into producto (product_name, product_img, product_cost, product_status, product_description, product_rate, product_category) 
+values ('Bandeja en forma de conejo','data:image/png;base64,', 100.00,  'Activo', 'Bandeja con diseño de conejo elaborada 100% madera y barnizada.', 3, 2);
+insert into producto (product_name, product_img, product_cost, product_status, product_description, product_rate, product_category) 
+values ('Base para cuchillos', 'data:image/png;base64,', 250.00,  'Activo', 'Porta Cuchillos hecho de madera de pino de primera calidad.', 2, 4);
+insert into producto (product_name, product_img, product_cost, product_status, product_description, product_rate, product_category) 
+values ('Base para audífonos', 'data:image/png;base64,', 500.00, 'Activo', 'Base para audífonos y/o auriculares hecho 100% de madera.', 3, 1);
+insert into producto (product_name, product_img, product_cost, product_status, product_description, product_rate, product_category) 
+values ('Porta Vaso', 'data:image/png;base64,', 50.00,  'Activo', 'Viste tu mesa y tus bebidas con este paquete de 4 portavasos. Con diseño de rompecabezas.', 4, 4);
 -- Insert de tipos de usuario
-insert into tipo_usuario (nombre, descripcion) values ('admin','Administrador de la pagina');
-insert into tipo_usuario (nombre, descripcion) values ('user','Usuario de la pagina');
+insert into tipo_usuario (nombre) values ('usuario');
+insert into tipo_usuario (nombre) values ('administrador');
 -- Insert de usuarios
-insert into usuario ( nombre, apellido_materno, apellido_paterno, correo, contrasena, telefono, fecha_nacimiento, tipo_usuario_id) 
-values ('Marihan Itzel', 'Bernal', 'Flores', 'marihan.bernal@gmail.com', 'Onedirection ', '4521122757', '1999-10-22', 1);
-insert into usuario (nombre, apellido_materno, apellido_paterno, correo, contrasena, telefono, fecha_nacimiento, tipo_usuario_id) 
-values ('Eduardo Miguel', 'Victoria', 'Sanchez', 'eduardo.mvisa@gmail.com', '123456789', '9612294249', '1999-09-20', 2);
-insert into usuario (nombre, apellido_materno, apellido_paterno, correo, contrasena, telefono, fecha_nacimiento, tipo_usuario_id) 
-values ('Armando', 'Elizalde', 'Santillan', 'eli_llan0dn4mr4@hotmail.com', '123456789', '7226671664', '1993-11-09', 2);
-insert into usuario (nombre, apellido_materno, apellido_paterno, correo, contrasena, telefono, fecha_nacimiento, tipo_usuario_id) 
-values ('Uziel', 'Munoz', 'Flores', 'Uzielmunozflores@gmail.com', '123456789', '3327628672', '2001-11-01', 2);
-insert into usuario (nombre, apellido_materno, apellido_paterno, correo, contrasena, telefono, fecha_nacimiento, tipo_usuario_id) 
-values ('Jose Luis', 'Vaca', 'Negrete', 'jluis_vakn@hotmail.com', '123456789', '4561033903', '1996-11-12', 2);
+insert into usuario (user_name, user_lastNF, user_lastNM, user_date, user_pass, user_phone, user_email, user_type_id) 
+values ('Marihan Itzel', 'Bernal', 'Flores', '1999-10-22', 'Onedirection', '4521122757', 'marihan.bernal@gmail.com', 2);
+insert into usuario (user_name, user_lastNF, user_lastNM, user_date, user_pass, user_phone, user_email, user_type_id) 
+values ('Eduardo Miguel', 'Victoria', 'Sanchez', '1999-09-20', '123456789', '9612294249', 'eduardo.mvisa@gmail.com', 1);
+insert into usuario (user_name, user_lastNF, user_lastNM, user_date, user_pass, user_phone, user_email, user_type_id) 
+values ('Armando', 'Elizalde', 'Santillan', '1993-11-09', '123456789', '7226671664', 'eli_llan0dn4mr4@hotmail.com', 1);
+insert into usuario (user_name, user_lastNF, user_lastNM, user_date, user_pass, user_phone, user_email, user_type_id) 
+values ('Uziel', 'Munoz', 'Flores', '2001-11-01','123456789', '3327628672', 'Uzielmunozflores@gmail.com', 1);
+insert into usuario (user_name, user_lastNF, user_lastNM, user_date, user_pass, user_phone, user_email, user_type_id) 
+values ('Jose Luis', 'Vaca', 'Negrete', '1996-11-12', '123456789', '4561033903', 'jluis_vakn@hotmail.com', 1);
--- bd/maderas&cncBd.sql
+++ bd/maderas&cncBd.sql
@@ -5,153 +5,115 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema maderas&cnc
+-- Schema maderacncdb
 -- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `maderacncdb` DEFAULT CHARACTER SET utf8mb3 ;
+USE `maderacncdb` ;
 
 -- -----------------------------------------------------
--- Schema maderas&cnc
+-- Table `maderacncdb`.`categorias`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `maderas&cnc` DEFAULT CHARACTER SET utf8 ;
-USE `maderas&cnc` ;
+CREATE TABLE IF NOT EXISTS `maderacncdb`.`categorias` (
+  `category_id` INT NOT NULL AUTO_INCREMENT,
+  `category_name` VARCHAR(20) NOT NULL,
+  `category_description` VARCHAR(60) NULL DEFAULT NULL,
+  PRIMARY KEY (`category_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb3;
+
 
 -- -----------------------------------------------------
--- Table `maderas&cnc`.`tipo_usuario`
+-- Table `maderacncdb`.`tipo_usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`tipo_usuario` (
+CREATE TABLE IF NOT EXISTS `maderacncdb`.`tipo_usuario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb3;
+-- -----------------------------------------------------
+-- Table `maderacncdb`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `maderacncdb`.`usuario` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `user_lastNF` VARCHAR(45) NOT NULL,
+  `user_lastNM` VARCHAR(45) NOT NULL,
+  `user_date` DATE NOT NULL,
+  `user_pass` VARCHAR(150) NOT NULL,
+  `user_phone` VARCHAR(45) NOT NULL,
+  `user_email` VARCHAR(100) NOT NULL,
+  `user_type_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `user_type_id`),
+  INDEX `fk_usuario_tipo_usuario_idx` (`user_type_id` ASC) VISIBLE,
+  CONSTRAINT `fk_usuario_tipo_usuario`
+    FOREIGN KEY (`user_type_id`)
+    REFERENCES `maderacncdb`.`tipo_usuario` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `maderas&cnc`.`usuario`
+-- Table `maderacncdb`.`direccion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`usuario` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido_materno` VARCHAR(45) NOT NULL,
-  `apellido_paterno` VARCHAR(45) NOT NULL,
-  `correo` VARCHAR(100) NOT NULL,
-  `contrasena` VARCHAR(150) NOT NULL,
-  `telefono` VARCHAR(45) NOT NULL,
-  `fecha_nacimiento` DATE NULL,
-  `tipo_usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id_usuario`, `tipo_usuario_id`),
-  INDEX `fk_usuario_tipo_usuario1_idx` (`tipo_usuario_id` ASC) VISIBLE,
-  CONSTRAINT `fk_usuario_tipo_usuario1`
-    FOREIGN KEY (`tipo_usuario_id`)
-    REFERENCES `maderas&cnc`.`tipo_usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `maderas&cnc`.`categorias`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`categorias` (
-  `id_categorias` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id_categorias`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `maderas&cnc`.`producto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`producto` (
-  `id_producto` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `calificacion` INT NOT NULL,
-  `precio` FLOAT NOT NULL,
-  `imagen` LONGBLOB NOT NULL,
-  `estatus` VARCHAR(15) NOT NULL,
-  `descripcion` VARCHAR(200) NOT NULL,
-  `categorias_idcategorias` INT NOT NULL,
-  PRIMARY KEY (`id_producto`, `categorias_idcategorias`),
-  INDEX `fk_producto_categorias1_idx` (`categorias_idcategorias` ASC) VISIBLE,
-  CONSTRAINT `fk_producto_categorias1`
-    FOREIGN KEY (`categorias_idcategorias`)
-    REFERENCES `maderas&cnc`.`categorias` (`id_categorias`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `maderas&cnc`.`imagen`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`imagen` (
-  `id_imagen` INT NOT NULL AUTO_INCREMENT,
-  `id_producto` INT NOT NULL,
-  `img` LONGBLOB NOT NULL,
-  PRIMARY KEY (`id_imagen`),
-  INDEX `fk_imagen_producto_idx` (`id_producto` ASC) VISIBLE,
-  CONSTRAINT `fk_imagen_producto`
-    FOREIGN KEY (`id_producto`)
-    REFERENCES `maderas&cnc`.`producto` (`id_producto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `maderas&cnc`.`orden`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`orden` (
-  `id_orden` INT NOT NULL,
-  `costo` FLOAT NOT NULL,
-  `clientes_id_cliente` INT NOT NULL,
-  `fecha_compra` DATE NOT NULL,
-  `estatus` VARCHAR(45) NOT NULL,
-  `ordencol` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_orden`, `clientes_id_cliente`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `maderas&cnc`.`detalles_orden`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`detalles_orden` (
-  `id` INT NOT NULL,
-  `cantidad` INT NOT NULL,
-  `importe` DECIMAL NOT NULL,
-  `orden_id_orden` INT NOT NULL,
-  `orden_clientes_id_cliente` INT NOT NULL,
-  PRIMARY KEY (`id`, `orden_id_orden`, `orden_clientes_id_cliente`),
-  INDEX `fk_detalles_orden_orden1_idx` (`orden_id_orden` ASC, `orden_clientes_id_cliente` ASC) VISIBLE,
-  CONSTRAINT `fk_detalles_orden_orden1`
-    FOREIGN KEY (`orden_id_orden` , `orden_clientes_id_cliente`)
-    REFERENCES `maderas&cnc`.`orden` (`id_orden` , `clientes_id_cliente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `maderas&cnc`.`direccion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `maderas&cnc`.`direccion` (
-  `id_direccion` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `maderacncdb`.`direccion` (
+  `id_direccion` INT NOT NULL AUTO_INCREMENT,
   `calle` VARCHAR(45) NOT NULL,
   `numero` VARCHAR(8) NOT NULL,
-  `colonia` VARCHAR(45) NULL,
-  `cp` VARCHAR(5) NULL,
+  `colonia` VARCHAR(45) NULL DEFAULT NULL,
+  `cp` VARCHAR(5) NULL DEFAULT NULL,
   `ciudad` VARCHAR(45) NOT NULL,
   `estado` VARCHAR(19) NOT NULL,
-  `referencia` VARCHAR(45) NULL,
-  `usuario_id_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_direccion`),
-  INDEX `fk_direccion_usuario1_idx` (`usuario_id_usuario` ASC) VISIBLE,
+  `referencia` VARCHAR(60) NULL DEFAULT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id_direccion`, `user_id`),
+  INDEX `fk_direccion_usuario1_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_direccion_usuario1`
-    FOREIGN KEY (`usuario_id_usuario`)
-    REFERENCES `maderas&cnc`.`usuario` (`id_usuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`user_id`)
+    REFERENCES `maderacncdb`.`usuario` (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `maderacncdb`.`producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `maderacncdb`.`producto` (
+  `product_id` INT NOT NULL AUTO_INCREMENT,
+  `product_name` VARCHAR(45) NOT NULL,
+  `product_img` TEXT NOT NULL,
+  `product_cost` FLOAT NOT NULL,
+  `product_status` VARCHAR(15) NOT NULL,
+  `product_description` VARCHAR(500) NOT NULL,
+  `product_rate` INT NOT NULL,
+  `product_category` INT NOT NULL,
+  PRIMARY KEY (`product_id`, `product_category`),
+  INDEX `fk_producto_categorias1_idx` (`product_category` ASC) VISIBLE,
+  CONSTRAINT `fk_producto_categorias1`
+    FOREIGN KEY (`product_category`)
+    REFERENCES `maderacncdb`.`categorias` (`category_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `maderacncdb`.`imagen`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `maderacncdb`.`imagen` (
+  `id_imagen` INT NOT NULL AUTO_INCREMENT,
+  `img` TEXT NOT NULL,
+  `product_id` INT NOT NULL,
+  PRIMARY KEY (`id_imagen`, `product_id`),
+  INDEX `fk_imagen_producto1_idx` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `fk_imagen_producto1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `maderacncdb`.`producto` (`product_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
